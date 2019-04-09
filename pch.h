a04cf850afb3ef6e3fce697f8b62afe44bc16346
imnetcat <guskov.danil@gmail.com>
2019-04-09 21:48:41 +0300

    Debagg

--- pch.h
+++ pch.h
@@ -6,16 +6,9 @@
 #include <WinSock2.h> // Для сокетов
 #include <ws2tcpip.h> // Для сокетов
 #include <iphlpapi.h> // Для GetIpAddrTable
-
+#include <wsipv6ok.h> // inet_pton & inet_ntop
 #pragma comment(lib, "Ws2_32.lib") // Для сокетов
 #pragma comment(lib, "Iphlpapi.lib") // Для GetIpAddrTable
-
-#ifdef __GNUC__
-   extern "C" {
-   WINSOCK_API_LINKAGE  INT WSAAPI inet_pton( INT Family, PCSTR pszAddrString, PVOID pAddrBuf);
-   WINSOCK_API_LINKAGE  PCSTR WSAAPI inet_ntop(INT  Family, PVOID pAddr, PSTR pStringBuf, size_t StringBufSize);
-   }
-#endif
 
 using namespace std;
 
